67a69ca1a7488aca830228b1fed15a75d3ae3930
John Clause <john.clause@gmail.com>
2015-02-16 19:59:36 -0700

    initial upload

---
+++ readme.txt
@@ -0,0 +1,43 @@
+=== WooCommerce & qTranslate-X ===
+Developed by: qTranslate Team
+Contributors: johnclause
+Tags: multilingual, language, bilingual, i18n, l10n, multilanguage, translation, WooCommerce
+Requires at least: 4.0
+Tested up to: 4.1
+Stable tag: 1.0
+License: GPLv3 or later
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=QEXEK3HX8AR6U
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+Enables multilingual framework for plugin "WooCommerce - excelling eCommerce".
+
+== Description ==
+
+Enables [qTranslate-X](https://wordpress.org/plugins/qtranslate-x/) multilingual framework for plugin [WooCommerce - excelling eCommerce](https://wordpress.org/plugins/woocommerce/).
+
+Steps to configure:
+
+* In all customizable template files, replace calls to `bloginfo('xxx')` with `bloginfo('xxx','display')`.
+
+== Installation ==
+
+Standard, as any other normal plugin hosted at WordPress.
+
+== Screenshots ==
+
+Plugin does not have any configuration options, and no screenshots needed.
+
+== Frequently Asked Questions ==
+
+= How do I open a page with configuration options? = 
+
+Plugin does not have any configuration options, simply activate it and it will enable the translation of relevant fields for WooCommerce back- and front-end.
+
+== Upgrade Notice ==
+
+No need for Upgrade Notice.
+
+== Changelog ==
+
+= 1.0 =
+* Initial release
---
+++ woocommerce-qtranslate-x.php
@@ -0,0 +1,209 @@
+<?php
+/**
+ * Plugin Name: WooCommerce & qTranslate-X
+ * Plugin URI: https://wordpress.org/plugins/woocommerce-qtranslate-x
+ * Description: Enables multilingual framework for plugin "WooCommerce".
+ * Version: 1.0
+ * Author: qTranslate Team
+ * Author URI: http://qtranslatexteam.wordpress.com/about
+ * License: GPL2
+ * Tags: multilingual, multi, language, translation, qTranslate-X, Events Made Easy
+ * Author e-mail: qTranslateTeam@gmail.com
+ */
+
+if ( ! function_exists( 'add_filter' ) ) {
+	header( 'Status: 403 Forbidden' );
+	header( 'HTTP/1.1 403 Forbidden' );
+	exit();
+}
+
+define('QWC_VERSION','1.0');
+
+if(is_admin()) :
+
+add_filter('qtranslate_load_admin_page_config','qwc_add_admin_page_config');
+function qwc_add_admin_page_config($page_configs)
+{
+	{//tab=tax
+	$page_config = array();
+	//$page_config['pages'] = array('admin.php' => 'page=wc-settings');
+	//$page_config['pages'] = array( 'admin.php' => 'page=wc-settings&tab=tax','admin.php' => 'page=wc-settings&tab=checkout' );
+	//$page_config['pages'] = array( 'admin.php' => 'page=wc-settings&tab=(tax|checkout)');
+	$page_config['pages'] = array( 'admin.php' => 'page=wc-settings&tab=tax');
+	//$page_config['anchors'] = array( 'titlediv'  );
+
+	$page_config['forms'] = array();
+
+	$f = array();
+	$f['form'] = array( 'id' => 'mainform' );
+
+	$f['fields'] = array();
+	$fields = &$f['fields']; // shorthand
+
+	//$fields[] = array( 'id' => 'woocommerce_tax_classes' );//todo
+	$fields[] = array( 'id' => 'woocommerce_price_display_suffix' );
+	//$fields[] = array( 'id' => '' );
+
+	$page_config['forms'][] = $f;
+	$page_configs[] = $page_config;
+	}
+
+	{//tab=checkout&section=wc_gateway_bacs
+	$page_config = array();
+	$page_config['pages'] = array( 'admin.php' => 'page=wc-settings&tab=checkout&section=wc_gateway_bacs');
+	//$page_config['anchors'] = array( 'titlediv'  );
+
+	$page_config['forms'] = array();
+
+	$f = array();
+	$f['form'] = array( 'id' => 'mainform' );
+
+	$f['fields'] = array();
+	$fields = &$f['fields']; // shorthand
+
+	$fields[] = array( 'id' => 'woocommerce_bacs_title' );
+	$fields[] = array( 'id' => 'woocommerce_bacs_description' );
+	$fields[] = array( 'id' => 'woocommerce_bacs_instructions' );
+	//$fields[] = array( 'id' => '' );
+
+	$page_config['forms'][] = $f;
+	$page_configs[] = $page_config;
+	}
+
+	{//tab=checkout&section=wc_gateway_cheque
+	$page_config = array();
+	$page_config['pages'] = array( 'admin.php' => 'page=wc-settings&tab=checkout&section=wc_gateway_cheque');
+	//$page_config['anchors'] = array( 'titlediv'  );
+
+	$page_config['forms'] = array();
+
+	$f = array();
+	$f['form'] = array( 'id' => 'mainform' );
+
+	$f['fields'] = array();
+	$fields = &$f['fields']; // shorthand
+
+	$fields[] = array( 'id' => 'woocommerce_cheque_title' );
+	$fields[] = array( 'id' => 'woocommerce_cheque_description' );
+	$fields[] = array( 'id' => 'woocommerce_cheque_instructions' );
+	//$fields[] = array( 'id' => '' );
+
+	$page_config['forms'][] = $f;
+	$page_configs[] = $page_config;
+	}
+
+	{//tab=checkout&section=wc_gateway_cod
+	$page_config = array();
+	$page_config['pages'] = array( 'admin.php' => 'page=wc-settings&tab=checkout&section=wc_gateway_cod');
+	//$page_config['anchors'] = array( 'titlediv'  );
+
+	$page_config['forms'] = array();
+
+	$f = array();
+	$f['form'] = array( 'id' => 'mainform' );
+
+	$f['fields'] = array();
+	$fields = &$f['fields']; // shorthand
+
+	$fields[] = array( 'id' => 'woocommerce_cod_title' );
+	$fields[] = array( 'id' => 'woocommerce_cod_description' );
+	$fields[] = array( 'id' => 'woocommerce_cod_instructions' );
+	//$fields[] = array( 'id' => '' );
+
+	$page_config['forms'][] = $f;
+	$page_configs[] = $page_config;
+	}
+
+	{//tab=checkout&section=wc_gateway_paypal
+	$page_config = array();
+	$page_config['pages'] = array( 'admin.php' => 'page=wc-settings&tab=checkout&section=wc_gateway_paypal');
+	//$page_config['anchors'] = array( 'titlediv'  );
+
+	$page_config['forms'] = array();
+
+	$f = array();
+	$f['form'] = array( 'id' => 'mainform' );
+
+	$f['fields'] = array();
+	$fields = &$f['fields']; // shorthand
+
+	$fields[] = array( 'id' => 'woocommerce_paypal_title' );
+	$fields[] = array( 'id' => 'woocommerce_paypal_description' );
+	$fields[] = array( 'id' => 'woocommerce_paypal_instructions' );
+	//$fields[] = array( 'id' => '' );
+
+	$page_config['forms'][] = $f;
+	$page_configs[] = $page_config;
+	}
+
+	{//tab=email
+	$page_config = array();
+	$page_config['pages'] = array( 'admin.php' => 'page=wc-settings&tab=email(&section=|)');
+	//$page_config['anchors'] = array( 'titlediv'  );
+
+	$page_config['forms'] = array();
+
+	$f = array();
+	$f['form'] = array( 'id' => 'mainform' );
+
+	$f['fields'] = array();
+	$fields = &$f['fields']; // shorthand
+
+	$fields[] = array( 'id' => 'woocommerce_email_from_name' );
+	//$fields[] = array( 'id' => 'woocommerce_email_header_image' );
+	$fields[] = array( 'id' => 'woocommerce_email_footer_text' );
+	//$fields[] = array( 'id' => '' );
+
+	$page_config['forms'][] = $f;
+	$page_configs[] = $page_config;
+	}
+
+	{//tab=email&section=wc_email_new_order
+	$page_config = array();
+	$page_config['pages'] = array( 'admin.php' => 'page=wc-settings&tab=email&section=wc_email_new_order');
+	//$page_config['anchors'] = array( 'titlediv'  );
+
+	$page_config['forms'] = array();
+
+	$f = array();
+	$f['form'] = array( 'id' => 'mainform' );
+
+	$f['fields'] = array();
+	$fields = &$f['fields']; // shorthand
+
+	$fields[] = array( 'id' => 'woocommerce_new_order_recipient' );
+	$fields[] = array( 'id' => 'woocommerce_new_order_subject' );
+	$fields[] = array( 'id' => 'woocommerce_new_order_heading' );
+	//$fields[] = array( 'id' => '' );
+
+	$page_config['forms'][] = $f;
+	$page_configs[] = $page_config;
+	}
+
+	return $page_configs;
+}
+
+else:
+
+add_filter('woocommerce_gateway_title', 'qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage', 0);
+add_filter('woocommerce_gateway_description', 'qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage', 0);
+add_filter('woocommerce_gateway_icon', 'qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage', 0);
+
+add_filter('woocommerce_shipping_methods', 'qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage', 0);
+
+add_filter('woocommerce_attribute_label', 'qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage', 0);
+
+//add_filter('woocommerce_in_cart_product_title', 'qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage', 0);
+add_filter('woocommerce_cart_item_name', 'qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage', 0);
+add_filter('woocommerce_cart_item_thumbnail', 'qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage', 0);
+
+//add_filter('woocommerce_order_table_product_title', 'qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage', 0);
+add_filter('woocommerce_order_items_table', 'qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage', 0);//object
+add_filter('woocommerce_order_item_name', 'qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage', 0);
+
+add_filter( 'woocommerce_email_get_option', 'qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage', 20 );
+
+add_filter( 'wp_mail_from_name', 'qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage', 20 );
+
+endif;
+?>
